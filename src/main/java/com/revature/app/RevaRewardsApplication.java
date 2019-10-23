e0ba3b79c63198ad22be7895f031a56d208e3d1c
maceris <KingOfL33tGaming@gmail.com>
2019-10-23 15:31:57 -0400

    Wipe and create new project

---
+++ src/main/java/com/revature/app/RevaRewardsApplication.java
@@ -0,0 +1,21 @@
+package com.revature.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point for the Spring application.
+ */
+@SpringBootApplication
+public class RevaRewardsApplication {
+
+	/**
+	 * Runs the spring application.
+	 * 
+	 * @param args passed to the spring application.
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(RevaRewardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/app/RevaRewardsApplicationTests.java
@@ -0,0 +1,18 @@
+package com.revature.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Tests the application.
+ */
+@SpringBootTest
+class RevaRewardsApplicationTests {
+
+	/**
+	 * Checks that the context loads. Was here by default.
+	 */
+	@Test
+	void contextLoads() {}
+
+}
