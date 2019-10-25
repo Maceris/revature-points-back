1ece133b94f3c6e3b22f1e9fa4953170894815c5
Jordan Zerella <jordan.zerella@yahoo.com>
2019-10-25 12:27:48 -0400

    added code for entity classes

--- src/main/java/com/revature/entities/Trainer.java
+++ src/main/java/com/revature/entities/Trainer.java
@@ -1,5 +1,59 @@
 package com.revature.entities;
 
 public class Trainer {
+	private int trainerId;
+	private String username;
+	private String password;
+	private String fname;
+	private String lname;
+	
+	public Trainer() {
+		super();
+	}
 
+	public int getTrainerId() {
+		return trainerId;
+	}
+
+	public void setTrainerId(int trainerId) {
+		this.trainerId = trainerId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	@Override
+	public String toString() {
+		return "Trainer [trainerId=" + trainerId + ", username=" + username + ", password=" + password + ", fname="
+				+ fname + ", lname=" + lname + "]";
+	}
 }
