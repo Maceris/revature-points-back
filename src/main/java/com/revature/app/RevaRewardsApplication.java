ba2cf02463844ecc83251e08be060fc4e6ec4208
maceris <KingOfL33tGaming@gmail.com>
2019-11-01 14:58:34 -0400

    remove the shutdown prevention

--- src/main/java/com/revature/app/RevaRewardsApplication.java
+++ src/main/java/com/revature/app/RevaRewardsApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.security.Permission;
-
 /**
  * The entry point for the Spring application.
  */
@@ -18,19 +16,6 @@ import java.security.Permission;
 public class RevaRewardsApplication {
 
 	public static void main(String[] args) {
-		forbidSystemExitCall();
 		SpringApplication.run(RevaRewardsApplication.class, args);
-	}
-
-	private static void forbidSystemExitCall() {
-		final SecurityManager securityManager = new SecurityManager() {
-			@Override
-			public void checkPermission(Permission permission) {
-				if (permission.getName().startsWith("exitVM")) {
-					throw new RuntimeException("Something called exit ");
-				}
-			}
-		};
-		System.setSecurityManager(securityManager);
 	}
 }
