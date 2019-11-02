4e1edc9a82b1aac1892ffd4a659d30d498faee2c
jzerella <zerellajordan@gmail.com>
2019-11-02 17:38:19 -0400

    Added cors filter

--- src/main/java/com/revature/app/RevaRewardsApplication.java
+++ src/main/java/com/revature/app/RevaRewardsApplication.java
@@ -3,8 +3,12 @@ package com.revature.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * The entry point for the Spring application.
@@ -23,4 +27,22 @@ public class RevaRewardsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RevaRewardsApplication.class, args);
 	}
+	
+	@Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
